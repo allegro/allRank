1077099629dbc4a648f04a7dc2b968c07f5dfad4
Tomasz Bartczak <tomasz.bartczak@allegro.pl>
2020-01-23 09:02:22 +0100

    update to python 3.7 and all deps

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@
 from setuptools import setup, find_packages
 
 reqs = [
-    "tensorflow==1.13.1",
-    "torch==1.2.0",
-    "torchvision==0.4.0",
-    "scikit-learn==0.21.3",
-    "pandas==0.24.2",
-    "numpy==1.16.1",
-    "scipy>=1.2.0",
-    "attrs==18.2.0",
-    "flatten_dict==0.1.0",
-    "pillow==6.2.1"
+    "tensorflow==1.15.0",
+    "torch==1.4.0",
+    "torchvision==0.5.0",
+    "scikit-learn==0.22.1",
+    "pandas==0.25.3",
+    "numpy==1.18.1",
+    "scipy==1.4.1",
+    "attrs==19.3.0",
+    "flatten_dict==0.2.0"
 ]
 
 setup(
     name="allRank",
-    version="1.1.0",
+    version="1.2.0",
     license="Apache 2",
     url="https://github.com/allegro/allRank",
     install_requires=reqs,
