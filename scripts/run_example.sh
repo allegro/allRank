13c2005bff06e71b54381f286d3d1ed9c45f7f35
Przemyslaw Pobrotyn <przemyslaw.pobrotyn@allegro.pl>
2020-04-07 17:34:39 +0200

    fix config path and rename output to job-dir in run_example.sh

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -4,4 +4,4 @@ DIR=$(dirname $0)
 PROJECT_DIR="$(cd $DIR/..; pwd)"
 
 docker build -t allrank:latest $PROJECT_DIR
-docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/data/generate_dummy_data.py && python allrank/main.py --config-file-name allrank/config.json --run-id test_run --output /allrank/test_run'
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/data/generate_dummy_data.py && python allrank/main.py --config-file-name /allrank/scripts/local_config.json --run-id test_run --job-dir /allrank/test_run'
