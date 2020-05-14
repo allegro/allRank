505498fbbe7e1854c63479b58cb3ca98344707a0
Tomasz Bartczak <tomasz.bartczak@allegro.pl>
2020-05-14 14:07:43 +0200

    polished docs in click_models.base

--- allrank/click_models/base.py
+++ allrank/click_models/base.py
@@ -1,17 +1,21 @@
+import math
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import List, Tuple, Callable
 
 import numpy as np
 
 
 class ClickModel(ABC):
+    """
+    Base class for all click models. Specifies the contract to be delivered
+    """
 
     @abstractmethod
-    def click(self, documents: Tuple[np.ndarray, np.ndarray]):
+    def click(self, documents: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
         """
-        return the mask for documents 1 if clicked, else 0.
+        applies a click model and returns the mask for documents.
 
-        :rtype: np.ndarray [ number_of_clicks ] -> a 0/1/-1 mask of length same as documents -
+        :rtype: np.ndarray [ number_of_documents ] -> a  mask of length same as documents -
         representing whether document was clicked (1) or not (0) or remained masked (-1)
 
         :param documents: Tuple of :
@@ -23,14 +27,17 @@ class ClickModel(ABC):
 
 class RandomClickModel(ClickModel):
     """
-    this ClickModel clicks on a random list of documents with a fixed number specified as a param
-
+    this ClickModel clicks a configured number of times on a random documents
     """
 
-    def __init__(self, n_clicks):
+    def __init__(self, n_clicks: int):
+        """
+
+        :param n_clicks: number of documents that will be clicked
+        """
         self.n_clicks = n_clicks
 
-    def click(self, documents):
+    def click(self, documents: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
         X, y = documents
         clicks = np.random.choice(range(len(y)), size=self.n_clicks, replace=False)
         mask = np.repeat(0, len(y))
@@ -40,14 +47,18 @@ class RandomClickModel(ClickModel):
 
 class FixedClickModel(ClickModel):
     """
-    this ClickModel clicks on a fixed list of documents specified as a param
+    this ClickModel clicks on a documents at fixed positions
 
     """
 
-    def __init__(self, click_positions):
+    def __init__(self, click_positions: List[int]):
+        """
+
+        :param click_positions: list of indices of documents that will be clicked
+        """
         self.click_positions = click_positions
 
-    def click(self, documents):
+    def click(self, documents: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
         X, y = documents
         clicks = np.repeat(0, len(y))
         clicks[self.click_positions] = 1
@@ -55,19 +66,24 @@ class FixedClickModel(ClickModel):
 
 
 class MultipleClickModel(ClickModel):
+    """
+    This click model is delegating to one from given click models with given probability
+    """
 
-    def __init__(self, click_models, probabilities):
+    def __init__(self, click_models: List[ClickModel], probabilities: List[float]):
+        """
+
+        :param click_models: list of candidate click models
+        :param probabilities: list of probabilities - must be of the same length as list of click models and sum to 1.0
+        """
         self.click_models = click_models
-        assert np.sum(probabilities) >= 0.99999 and np.sum(
-            probabilities) <= 1.000001, "probabilities should sum to one, but got {} which sums to {}".format(
-            probabilities, np.sum(probabilities)
-        )
+        assert math.isclose(np.sum(probabilities), 1.0, abs_tol=1e-5), \
+            f"probabilities should sum to one, but got {probabilities} which sums to {np.sum(probabilities)}"
         self.probabilities = np.array(probabilities).cumsum()
 
-    def click(self, documents):
+    def click(self, documents: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
         index = np.argmax(np.random.rand() < self.probabilities)
         result = self.click_models[index].click(documents)
-        # print("finally model from index {} got {} clicks {}".format(index, np.sum(result, -1), result))
         return result
 
 
@@ -75,34 +91,38 @@ class ConditionedClickModel(ClickModel):
     """
         This click model allows to combine multiple click models with a logical funciton
 
+        """
+
+    def __init__(self, click_models: List[ClickModel], combiner: Callable):
+        """
+
         :param click_models: list of click models to combine
         :param combiner: a function applied to the result of clicks from click models - e.g. np.all or np.any
         """
-
-    def __init__(self, click_models, combiner):
         self.click_models = click_models
         self.combiner = combiner
 
-    def click(self, documents):
+    def click(self, documents: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
         clicks_from_click_models = [click_model.click(documents) for click_model in self.click_models]
         return self.combiner(clicks_from_click_models, 0)
 
 
-def combine_with_and(*click_models: ClickModel):
-    return ConditionedClickModel(click_models, np.all)
-
-
-def combine_with_or(click_models: List[ClickModel]):
-    return ConditionedClickModel(click_models, np.any)
-
-
 class MaxClicksModel(ClickModel):
+    """
+    This click model that takes other click model and limits the number of clicks to given value.
+    Effectively keeping top <max_clicks> clicks
+    """
 
-    def __init__(self, click_model, max_clicks):
+    def __init__(self, click_model: ClickModel, max_clicks: int):
+        """
+
+        :param click_model: a delegate click model to generate clicks
+        :param max_clicks: number of clicks that should be preserved
+        """
         self.click_model = click_model
         self.max_clicks = max_clicks
 
-    def click(self, documents):
+    def click(self, documents: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
         underlying_clicks = self.click_model.click(documents)
         if self.max_clicks is not None:
             max_clicks_mask = underlying_clicks.cumsum() <= self.max_clicks
@@ -112,14 +132,17 @@ class MaxClicksModel(ClickModel):
 
 class OnlyRelevantClickModel(ClickModel):
     """
-    this ClickModel clicks on a document:
-    1. when its relevancy is at least of predefined value
+    this ClickModel clicks on a document when its relevancy is at least of predefined value
 
     """
 
-    def __init__(self, relevancy_threshold=1):
+    def __init__(self, relevancy_threshold: float):
+        """
+
+        :param relevancy_threshold: a minimum value of relevancy of a document to be clicked (inclusive)
+        """
         self.relevancy_threshold = relevancy_threshold
 
-    def click(self, documents):
+    def click(self, documents: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
         X, y = documents
         return np.array(y) >= self.relevancy_threshold
